07689c9b9a5bd73fbb78ad1130b560c072c9ed3b
Casey-Page <caseypage@mail.com>
2025-02-08 12:09:13 +0000

    Create quiz context.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import Header from './components/Header';
 import Quiz from './components/Quiz';
 import questions from './questions';
 
+// TODO:
+// - Add a timer to the quiz
+// Create a model for end screen
+// - Add a score to the quiz
+// - Add a high score to the quiz
+// - Add a restart button to the quiz
+
+
+
 function App() {
     // Quiz state
     const [quizStarted, setQuizStarted] = useState(false);
---
+++ src/quiz/quiz-context.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from "react";
+
+// Quiz context
+const QuizContext = createContext();
+
+// Quiz provider
+export function QuizProvider({ children }) {
+    // Quiz state
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [quizFinished, setQuizFinished] = useState(false);
+    const [score, setScore] = useState(0);
+
+    // Quiz context value that will be used by the quiz component
+    const value = {
+        currentQuestion,
+        setCurrentQuestion,
+        quizFinished,
+        setQuizFinished,
+        score,
+        setScore
+    }
+    
+    return (
+        // Provide the quiz context value to the quiz component
+        <QuizContext.Provider value={ value}>
+            {children}
+        </QuizContext.Provider>
+    )
+}
+
+// Quiz hook to access the quiz context
+export function useQuiz() {
+    const context = useContext(QuizContext);
+    if (!context) {
+        throw new Error('useQuiz must be used within a QuizProvider');
+    }
+    return context;
+}
